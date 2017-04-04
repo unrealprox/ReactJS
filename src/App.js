230ae5511985a6b16d25b4ef9bfe1ec86f6948f8
Vladislav <unrealprox@gmail.com>
2017-04-05 02:13:03 +0300

    Game analyze

--- src/App.js
+++ src/App.js
@@ -8,9 +8,9 @@ class Square extends Component {
     }
 
     squareClick() {
-        if (this.state.value == "") {
+        if (this.state.value === "") {
             this.setState({ value : this.props.currentPlayer });
-            this.props.changeTurn();
+            this.props.changeTurn(this.props.row, this.props.col);
         }
     }
 
@@ -31,9 +31,9 @@ class Row extends Component {
     render() {
         return (
             <div className="Row">
-                <Square changeTurn={this.props.changeTurn} currentPlayer={this.props.currentPlayer}/>
-                <Square changeTurn={this.props.changeTurn} currentPlayer={this.props.currentPlayer}/>
-                <Square changeTurn={this.props.changeTurn} currentPlayer={this.props.currentPlayer}/>
+                <Square row={this.props.row} col={0} changeTurn={this.props.changeTurn} currentPlayer={this.props.currentPlayer}/>
+                <Square row={this.props.row} col={1} changeTurn={this.props.changeTurn} currentPlayer={this.props.currentPlayer}/>
+                <Square row={this.props.row} col={2} changeTurn={this.props.changeTurn} currentPlayer={this.props.currentPlayer}/>
             </div>
         );
     }
@@ -42,29 +42,102 @@ class Row extends Component {
 class App extends Component {
   constructor(props) {
       super(props);
-      this.state = { currentPlayer : "x", message : "X's turn" };
+      var field = new Array(9);
+      for (var i = 0; i < 9; ++i)
+          field[i] = 0;
+      this.state = {
+          currentPlayer : "x",
+          message : "X's turn",
+          field : field
+      };
       this.changeTurn = this.changeTurn.bind(this);
   }
 
-  changeTurn() {
-      let {currentPlayer, message} = this.state;
-      if (currentPlayer == "x") {
+  analyzeSum(sum) {
+      if (sum === 3)
+          return 1;
+      if (sum === -3)
+          return -1;
+      return 0;
+  }
+
+  analyzeWin(field) {
+      var i, j, sum, sumAnalyze;
+
+
+      for (i = 0; i < 3; ++i) {
+          sum = 0;
+          for (j = 0; j < 3; ++j)
+              sum += field[i * 3 + j];
+          sumAnalyze = this.analyzeSum(sum);
+          if (sumAnalyze !== 0)
+              return sumAnalyze;
+      }
+      for (i = 0; i < 3; ++i) {
+          sum = 0;
+          for (j = 0; j < 3; ++j)
+              sum += field[j * 3 + i];
+          sumAnalyze = this.analyzeSum(sum);
+          if (sumAnalyze !== 0)
+              return sumAnalyze;
+      }
+      sum = 0;
+      for (i = 0; i < 3; ++i)
+          sum += field[i * 3 + i];
+      sumAnalyze = this.analyzeSum(sum);
+      if (sumAnalyze !== 0)
+          return sumAnalyze;
+      sum = 0;
+      for (i = 0; i < 3; ++i)
+          sum += field[i * 3 + (2 - i)];
+      sumAnalyze = this.analyzeSum(sum);
+      if (sumAnalyze !== 0)
+          return sumAnalyze;
+
+      var empty = false;
+      for (i = 0; i < 9; ++i)
+          if (field[i] === 0) {
+            empty = true;
+            break;
+          }
+      if (!empty)
+          return 2;
+  }
+
+  changeTurn(row, col) {
+      let {currentPlayer, message, field} = this.state;
+      if (currentPlayer === "x") {
           currentPlayer = "o";
           message = "O's turn";
+          field[3 * row + col] = 1;
       } else {
           currentPlayer = "x";
           message = "X's turn";
+          field[3 * row + col] = -1;
       }
-      this.setState({ currentPlayer : currentPlayer, message : message });
+
+      var winAnalyze = this.analyzeWin(field);
+      if (winAnalyze === 1)
+          message = "X won";
+      else if (winAnalyze === -1)
+          message = "O won";
+      else if (winAnalyze === 2)
+          message = "Draw";
+
+      this.setState({
+          currentPlayer : currentPlayer,
+          message : message,
+          field : field
+      });
   }
 
   render() {
     return (
         <div>
             <div className="Field">
-                <Row changeTurn={this.changeTurn} currentPlayer={this.state.currentPlayer}/>
-                <Row changeTurn={this.changeTurn} currentPlayer={this.state.currentPlayer}/>
-                <Row changeTurn={this.changeTurn} currentPlayer={this.state.currentPlayer}/>
+                <Row row={0} changeTurn={this.changeTurn} currentPlayer={this.state.currentPlayer}/>
+                <Row row={1} changeTurn={this.changeTurn} currentPlayer={this.state.currentPlayer}/>
+                <Row row={2} changeTurn={this.changeTurn} currentPlayer={this.state.currentPlayer}/>
             </div>
             <div className="Message">
                 {this.state.message}
