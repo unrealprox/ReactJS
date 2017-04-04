1da4af22878654689dd24f7b67fd44867da50cfc
Vladislav <unrealprox@gmail.com>
2017-04-05 01:41:46 +0300

    Крестики-нолики (наработки)

--- src/App.js
+++ src/App.js
@@ -1,19 +1,67 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+class Square extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { value : "" };
+    }
+
+    squareClick() {
+        if (this.state.value == "") {
+            this.setState({ value : this.props.currentPlayer });
+            this.props.changeTurn();
+        }
+    }
+
+    render() {
+        return (
+            <div className="Square" onClick={this.squareClick.bind(this)}>
+                {this.state.value}
+            </div>
+        );
+    }
+}
+
+class Row extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div className="Row">
+                <Square changeTurn={this.props.changeTurn} currentPlayer={this.props.currentPlayer}/>
+                <Square changeTurn={this.props.changeTurn} currentPlayer={this.props.currentPlayer}/>
+                <Square changeTurn={this.props.changeTurn} currentPlayer={this.props.currentPlayer}/>
+            </div>
+        );
+    }
+}
+
 class App extends Component {
+  constructor(props) {
+      super(props);
+      this.state = { currentPlayer : "x" };
+      this.changeTurn = this.changeTurn.bind(this);
+  }
+
+  changeTurn() {
+      let {currentPlayer} = this.state;
+      if (currentPlayer == "x")
+          currentPlayer = "o";
+      else
+          currentPlayer = "x";
+      this.setState({ currentPlayer : currentPlayer});
+  }
+
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+        <div className="Field">
+            <Row changeTurn={this.changeTurn} currentPlayer={this.state.currentPlayer}/>
+            <Row changeTurn={this.changeTurn} currentPlayer={this.state.currentPlayer}/>
+            <Row changeTurn={this.changeTurn} currentPlayer={this.state.currentPlayer}/>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
     );
   }
 }
